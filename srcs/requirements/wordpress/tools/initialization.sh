6d51a7a478ef575cb5937d7efbcf6df5f6cd59be
afontele <amandafontelesprojects@gmail.com>
2026-07-28 16:33:19 +0200

    wordpress

---
+++ srcs/requirements/wordpress/tools/initialization.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sleep 10
+ #if wp-config.php doesn't exists
+wp config create --allow-root \
+				--dbname=$MDB_DB \
+				--dbuser=$MDB_USR \
+				--dbpass=$MDB_PSSWRD \
+				--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+wp core install
